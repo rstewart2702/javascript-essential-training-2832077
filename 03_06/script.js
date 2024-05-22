cfe75c4836d58de78e74b2dbdbbd0cb73dbcbc96
Richard S. Stewart <rstewart2702@gmail.com>
2024-05-22 12:31:30 -0500

    Notes/thoughts:  regarding accessing of object properties.

--- 03_06/script.js
+++ 03_06/script.js
@@ -22,3 +22,9 @@ const backpack = {
 };
 
 console.log("The backpack object:", backpack);
+
+// Just goes to show that "dot notation" is an analogous
+// to using string-names inside the "bracket-accessing" notation.
+// Makes "objects" which are rather like "dictionaries," i.e.,
+// probably implemented as a hash table under the covers?
+console.log(`backpack.pocketNum:  ${backpack["pocketNum"]}`); 
