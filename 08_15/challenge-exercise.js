0675c25abd4c9e47b2d0a2f87495c1c27a850bc5
Richard S. Stewart <rstewart2702@gmail.com>
2024-05-30 16:45:27 -0500

    Chapter 8 Challenge:  my work thus far...
    ...does not seem to be quite what was asked for?

--- 08_15/challenge-exercise.js
+++ 08_15/challenge-exercise.js
@@ -56,16 +56,36 @@ const backpackObjectArray = [everydayPack, frogPack];
 
 function createBackpackMarkup(document) {
     // Replace the code below with the correct solution:
-    const content = `
-    <h1>${everydayPack.name}</h1>
-    <ul>
-        <li>Volume:<span> ${everydayPack.volume}l</span></li>
-        <li>Color:<span> ${everydayPack.color}</span></li>
-        <li>Age:<span> ${everydayPack.dateAcquired} days old</span></li>
-        <li>Number of pockets:<span> ${everydayPack.pocketNum}</span></li>
-    </ul>
-    `;
-    return content;
+    //
+    // Used to derive the pack contents, per backpack:
+    let fcnPckContent =
+	(backpackItm) => {
+	    return `
+	    <h1>${backpackItm.name}</h1>
+	    <ul>
+		<li>Volume:<span> ${backpackItm.volume}l</span></li>
+		<li>Color:<span> ${backpackItm.color}</span></li>
+		<li>Age:<span> ${backpackItm.dateAcquired} days old</span></li>
+		<li>Number of pockets:<span> ${backpackItm.pocketNum}</span></li>
+	    </ul>`;
+	}
+    // The objective:  produce "serialized HTML fragments,"
+    // which implies that "real" HTML elements should be produced,
+    // the real data structure as opposed to the "flat" HTML text
+    // (which would have to be re-parsed, etc, right?)
+    let content = Array();
+    let pageElement = null;
+    backpackObjectArray.forEach(
+	(itm) => {
+	    pageElement = document.createElement("article");
+	    pageElement.id = itm.id;
+	    pageElement.innerHTML = fcnPckContent(itm);
+	    // console.log(pageElement.outerHTML);
+	    content.push(pageElement);
+	}
+    );
+    console.log(content);
+    return content; // thus content is an Array containing "article Element objects."
 }
 // This is how your code will be called.
 // Your answer should be the HTML output described in the challenge text.
@@ -76,7 +96,15 @@ const result = backpacksMarkupArray.reduce((combinedHTML, backpack) => {
     return combinedHTML + backpack.outerHTML;
 }, '');
 
+console.log(result)
 
+// The following "squirts" into the page's own structure the
+// "article objects" just derived and stored into the backpacksMarkupArray,
+// whose name is kind of misleading, for it is an array of
+// HTML page objects which've already been "marked up" as far as
+// I am concerned.
+backpacksMarkupArray.forEach((itm) => { document.body.append(itm); });
+document.body.appendChild(backpackListElt);
 
 
 /**
